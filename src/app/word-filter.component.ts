20582331aa12c70caad9e09044303fbbf8ddb01f
Van Pho Nguyen <vanpho02@gmail.com>
2018-03-26 19:58:44 +0700

    onChange

--- src/app/word-filter.component.ts
+++ src/app/word-filter.component.ts
@@ -10,6 +10,7 @@ import { Word, AppState } from './types';
             class="form-control"
             style="width: 200px"
             value="{{ filterStatus | async }}"
+            (change)="onChange($event);"
         >
             <option value="SHOW_ALL">SHOW ALL</option>
             <option value="SHOW_MEMORIZED">SHOW MEMORIZED</option>
@@ -22,5 +23,12 @@ export class WordFilterComponent {
     filterStatus: Observable<string>;
     constructor(private store: Store<AppState>) {
         this.filterStatus = this.store.select('filterStatus');
+    }
+
+    onChange(evt) {
+        this.store.dispatch({
+            type: 'SET_FILTER_STATUS',
+            filterStatus: evt.target.value
+        });
     }
 }
